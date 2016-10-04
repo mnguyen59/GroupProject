a8fe7a7b1479234a3e1ae642bbaef00ed808279f
My <mnguyen59@student.gsu.edu>
2016-10-04 18:23:24 -0400

    Initial check in of sales tool

---
+++ Class_Examples/src/mypackage/TV.java
@@ -0,0 +1,29 @@
+package mypackage;
+
+public class TV {	//was "chp8_classnotes"
+	
+	public int size;
+	public String color;
+	public boolean on;
+	public int volume;
+	
+	public TV(){
+		
+	}
+	
+	public TV(int s, String c, boolean b, int v){
+		
+		size = s;
+		color = c;
+		on = b;
+		volume = v;
+	}
+	
+	public void turnOn(){
+		on = true;
+	}
+	
+	public void turnOff(){
+		on = false;
+	}
+}
